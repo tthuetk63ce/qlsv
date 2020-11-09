6f781db62ea352531eb7d9c8e013a31712458cdb
tthuetk63ce <61744063+tthuetk63ce@users.noreply.github.com>
2020-11-09 20:21:26 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -8,6 +8,6 @@ if (mysqli_connect_errno())
 
 // Change character set to utf8
 mysqli_set_charset($conn,"utf8");
-$url = "http://localhost:8080/QLSV/";
+$url = "http://localhost:8080/qlsv/";
 error_reporting(0);
 ?>
