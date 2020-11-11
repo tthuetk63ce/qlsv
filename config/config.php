13a3c725f4098430a8e76562919c2d4aa59ee205
tthuetk63ce <61744063+tthuetk63ce@users.noreply.github.com>
2020-11-11 10:45:15 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("localhost","root","","ql_sinhvien");
+$conn=mysqli_connect("mysql-15150-0.cloudclusters.net","haitd123","25092000","ql_sinhvien","15150");
 // Check connection
 if (mysqli_connect_errno())
   {
@@ -8,6 +8,6 @@ if (mysqli_connect_errno())
 
 // Change character set to utf8
 mysqli_set_charset($conn,"utf8");
-$url = "http://localhost:8080/qlsv/";
+$url = "http://localhost/qlsv/";
 error_reporting(0);
 ?>
